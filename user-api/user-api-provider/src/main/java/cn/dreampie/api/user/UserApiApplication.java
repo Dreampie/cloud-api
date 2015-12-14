7ccdf4d8ef7caf22f6e53056f1d7bb1f6be52f2e
Dreampie <Dreampie@outlook.com>
2015-12-14 18:13:26 +0800

    add hystrix-dashboard

--- user-api/user-api-provider/src/main/java/cn/dreampie/api/user/UserApiApplication.java
+++ user-api/user-api-provider/src/main/java/cn/dreampie/api/user/UserApiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 @EnableFeignClients(basePackages = {"cn.dreampie.service"})
 @ComponentScan(basePackages = {"cn.dreampie.api"})
 public class UserApiApplication {
